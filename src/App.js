9e960faed001a615a14be4d4fc5e2c2297a1ea35
OktavianBaba <octavianjobs@gmail.com>
2022-07-09 19:04:40 +0200

    Amazon Clone project

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import Header from './Header';
+import Home from './Home';
+import Checkout from './Checkout';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    //BEM
+    <Router>
+    <div className="app">
+      <Routes>
+        <Route path="/checkout" element={[<Header/>,<Checkout/>]} />
+        <Route path="/" element={[<Header/>,<Home/>]} />
+    
+      </Routes>
+      
+    </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from "@material-ui/icons/Search";
+import  ShoppingBasketIcon  from '@material-ui/icons/ShoppingBasket';
+import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
+
+function Header() {
+    const [{basket}] = useStateValue();
+
+
+  return (
+    <div className="header">
+        <Link to="/">
+        <img 
+        className="header__logo" alt = "Amazon Logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
+        </Link>
+        
+
+        <div className="header__search">
+            <input className="header__searchInput" type="text" />
+            <SearchIcon
+            className="header__searchIcon" />
+        </div>
+
+        <div className="header__nav">
+            <div className="header__option">
+                <span className="header__optionLineoOne">Hello Guest</span>
+                <span className="header__optionLineoTwo">Sign In</span>
+            </div>
+
+            <div className="header__option">
+                <span className="header__optionLineoOne">Returns</span>
+                <span className="header__optionLineoTwo">& Orders</span>
+            </div>
+            <div className="header__option">
+            <span className="header__optionLineoOne">Your</span>
+                <span className="header__optionLineoTwo">Prime</span>
+            </div>
+        </div>
+
+        <Link to="/checkout">
+        <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__optionLineTwo header__basketCount">{basket?.length}</span>
+        </div>
+        </Link>
+      
+    </div>
+  )
+}
+
+export default Header
